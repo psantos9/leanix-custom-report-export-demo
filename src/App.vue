fb2f082e644c6d9cf9dfbf22fcb556c80367c352
Paulo Santos <paulo.santos@leanix.net>
2024-02-20 09:04:50 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Custom Report Export Demo</div>
+</template>
+
+<script lang="ts" setup>
+import '@leanix/reporting'
+;(async () => {
+  await lx.init()
+  const reportSetup: lxr.ReportConfiguration = {
+    export: {}
+  }
+  await lx.ready(reportSetup)
+})()
+</script>
